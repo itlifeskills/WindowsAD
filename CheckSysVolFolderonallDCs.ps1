b4f8263324fe2448b791f282767ba818efa74782
itlifeskills <106455363+itlifeskills@users.noreply.github.com>
2023-12-24 11:00:29 -0600

    Add files via upload

---
+++ CheckSysVolFolderonallDCs.ps1
@@ -0,0 +1,10 @@
+﻿$elgpvdc03Sysvol = "\\ELGPVDC03\sysvol\hq.itlifeskills.local\Policies"
+$aurpvdc01Sysvol = "\\AURPVDC01\sysvol\hq.itlifeskills.local\Policies"
+$chipvdc11Sysvol = "\\CHIPVDC11\sysvol\hq.itlifeskills.local\Policies"
+
+
+$sysVols = $elgpvdc03Sysvol, $aurpvdc01Sysvol, $chipvdc11Sysvol 
+
+$sysVols | Get-ChildItem 
+
+#New-GPO -Name "ITLifeSkills Computer Settings" -Server ELGPVDC03
