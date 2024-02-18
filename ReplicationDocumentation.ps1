94ae8e5e48313650db8af90e120b97013a442076
itlifeskills <106455363+itlifeskills@users.noreply.github.com>
2024-02-18 15:47:04 -0600

    Add files via upload

---
+++ ReplicationDocumentation.ps1
@@ -0,0 +1,16 @@
+﻿#Get all domain controllers in the AD forest
+$sourceDCs = ((Get-ADForest).Domains | %{ Get-ADDomainController -Filter * -Server $_ }).HostName
+
+#For each domain controller, find all domain controllers where it replicate to
+foreach($sourceDC in $sourceDCs){
+    $destinationDCs = (Get-ADReplicationConnection -Filter * | Select ReplicateFromDirectoryServer, ReplicateToDirectoryServer | 
+                           where{$_.ReplicateFromDirectoryServer.Contains($sourceDC.Split(".")[0])}).ReplicateToDirectoryServer
+    
+    #Put all destination domain controllers into a string to show their names only
+    $destinationDC = $null
+    foreach($dc in $destinationDCs){
+        $destinationDC += ($dc.Split(",")[0]).Split("=")[1] + "; "           
+    }
+    Write-Host "Replicating from $sourceDC to $destinationDC"
+
+}
