4d93929e02a0aa8c94280a15a369f1973c4d1a56
itlifeskills <106455363+itlifeskills@users.noreply.github.com>
2024-09-22 10:27:37 -0500

    Delete Create Organizational Units.ps1

--- Create Organizational Units.ps1
+++
@@ -1,7 +0,0 @@
-﻿$departments = (Import-Csv -Path "C:\Scripts\Data\Departments.csv").Department
-
-foreach ($department in $departments){
-
-    New-ADOrganizationalUnit -Name $department -Path "OU=Users,OU=ITLifeSkills,DC=hq,DC=itlifeskills,DC=local"
-}
-
